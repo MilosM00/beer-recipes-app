f656e762054430c31244a102be321bdc3089e1ea
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-22 12:15:16 +0200

    navbar-js updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,39 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () =>{
+const Navbar = ({hamburger, hamburgerEvent, scrollUp, scrollUpHamburger}) =>{
     return(
 
         <>
 
             <div className="navbar">
-                <p>BRA 🍺</p>
+                <p onClick={scrollUp}>BRA 🍺</p>
 
                 <ul>
-                    <li><p>Home</p></li>
+                    <li onClick={scrollUp}><p>Home</p></li>
                     <li><a href="https:/github.com">About</a></li>
                     <li><a href="https:/github.com">History</a></li>
                     <li><a href="https:/github.com">Recipes</a></li>
                 </ul>
+
+                <p 
+                    className="hamburger" 
+                    onClick={hamburgerEvent}
+                >
+                    {hamburger === true ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
+                </p>
             </div>
+
+            {hamburger === true &&
+                <div className="hamburger-list">
+                    <ul>
+                        <li onClick={scrollUpHamburger}><p><i className="fa fa-home"></i> Home</p></li>
+                        <li onClick={hamburgerEvent}><a href="https:/github.com"><i className="fa fa-question"></i> About</a></li>
+                        <li onClick={hamburgerEvent}><a href="https:/github.com"><i className="fa fa-shield"></i> History</a></li>
+                        <li onClick={hamburgerEvent}><a href="https:/github.com"><i className="fa fa-book"></i> Recipes</a></li>
+                    </ul>
+                </div>
+            }
             
         </>
 
