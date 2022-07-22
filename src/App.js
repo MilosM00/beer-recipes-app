4d0b556d7a5af8e8a7d5a4347a0603140065cb3e
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-22 12:14:33 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import React from "react";
 import Navbar from "./components/Navbar";
 
 const App = () =>{
+
+    const [hamburger, setHamburger] = React.useState(false);
+
+    const hamburgerEvent = () =>{
+        setHamburger(!hamburger);
+    };
+
+    const scrollUp = () =>{
+        document.body.scrollIntoView();
+    };
+    const scrollUpHamburger = () =>{
+        document.body.scrollIntoView();
+        setHamburger(!hamburger);
+    };
+
     return(
 
         <div>
 
-            <Navbar />
+            <Navbar 
+                hamburger={hamburger}
+                hamburgerEvent={hamburgerEvent}
+                scrollUp={scrollUp}
+                scrollUpHamburger={scrollUpHamburger}
+            />
 
         </div>
 
