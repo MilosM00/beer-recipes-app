edcd0dacd90421ad58e4281f06f0dc606dfa65d9
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-23 09:50:25 +0200

    home-js update

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () =>{
+    return(
+
+        <div className="home">
+            <div className="home-overlay">
+
+                <p className="title-home">Welcome Beer Drinker!</p>
+
+                <p className="info-home">
+                    Find out about the best recipes for beers!
+                </p>
+
+                <p className="info-home">
+                    Learn about beer and beer history!
+                </p>
+
+                <div className="inline-home">
+                    <a href="https://github.com">Learn More</a>
+                    <a href="https://github.com">Beer Recipes</a>
+                </div>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Home;
