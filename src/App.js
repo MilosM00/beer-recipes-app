ec25216d6ede8bb35b17ee96881a40211316966a
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-25 11:33:27 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 const App = () =>{
 
@@ -30,6 +31,8 @@ const App = () =>{
             />
 
             <Home />
+
+            <About />
 
         </div>
 
