f4195d2bb3c001c4ca9dbb0ee359d98016a846d2
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-29 11:14:48 +0200

    navbar-js updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,9 @@ const Navbar = ({hamburger, hamburgerEvent, scrollUp, scrollUpHamburger}) =>{
 
                 <ul>
                     <li onClick={scrollUp}><p>Home</p></li>
-                    <li><a href="https:/github.com">About</a></li>
-                    <li><a href="https:/github.com">History</a></li>
-                    <li><a href="https:/github.com">Recipes</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#history">History</a></li>
+                    <li><a href="#recipes">Recipes</a></li>
                 </ul>
 
                 <p 
@@ -28,9 +28,9 @@ const Navbar = ({hamburger, hamburgerEvent, scrollUp, scrollUpHamburger}) =>{
                 <div className="hamburger-list">
                     <ul>
                         <li onClick={scrollUpHamburger}><p><i className="fa fa-home"></i> Home</p></li>
-                        <li onClick={hamburgerEvent}><a href="https:/github.com"><i className="fa fa-question-circle"></i> About</a></li>
-                        <li onClick={hamburgerEvent}><a href="https:/github.com"><i className="fa fa-shield"></i> History</a></li>
-                        <li onClick={hamburgerEvent}><a href="https:/github.com"><i className="fa fa-book"></i> Recipes</a></li>
+                        <li onClick={hamburgerEvent}><a href="#about"><i className="fa fa-question-circle"></i> About</a></li>
+                        <li onClick={hamburgerEvent}><a href="#history"><i className="fa fa-shield"></i> History</a></li>
+                        <li onClick={hamburgerEvent}><a href="#recipes"><i className="fa fa-book"></i> Recipes</a></li>
                     </ul>
                 </div>
             }
